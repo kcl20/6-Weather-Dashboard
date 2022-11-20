890b3fe3b6fa4d03c6963d5b4f345fb581f49d33
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-11-20 16:15:03 -0500

    Working API calls for current weather.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,9 @@
 var searchInput = document.getElementById("citySearch");
-var searchButton = document.getElementById("searchButton");
+var searchButton = document.getElementById('searchButton');
 
-// Event listener for search button
-searchButton.addEventListener("click", function() {
-    console.log("click");
-    var city = searchInput.value;
-    console.log("Search for " + city);
-//   getLatLong(city);
-});
 
-function getLatLong() {
+function getLatLong(city) {
+    console.log("looking up lat/long for " + city);
     var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=9ca09e5fa97a5b754fdff55f82acba19";
     fetch(geoURL)
     .then(function(response) {
@@ -21,15 +15,35 @@ function getLatLong() {
         var lon = data[0].lon;
         getWeather(lat, lon);
     })
+    // todo: add validation if city not found
+    // todo:  add validation if error (status not 200)
 }
 
 function getWeather(lat, lon) {
-    var weatherURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=9ca09e5fa97a5b754fdff55f82acba19";
+    console.log("looking up weather for " + lat + ", " + lon);
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=9ca09e5fa97a5b754fdff55f82acba19";
     fetch(weatherURL)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
+        var currentTemp = data.main.temp;
+        var currentHumidity = data.main.humidity;
+        var currentWindSpeed = data.wind.speed;
+        
     })
 }
+
+
+// Event listener for search button
+searchButton.addEventListener("click", function() {
+    event.preventDefault();
+    var city = searchInput.value;
+    if (city==="") {
+        alert("Please enter a city");
+        return;}
+    console.log("click");
+    console.log("Search for " + city);
+  getLatLong(city);
+});
