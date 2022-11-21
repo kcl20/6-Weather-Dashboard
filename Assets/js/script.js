487e9caaa3f510d9739ed53cf229df9b42a3c586
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-11-20 21:25:16 -0500

    Current Weather functions working.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,7 @@ var searchButton = document.getElementById('searchButton');
 
 function getLatLong(city) {
     console.log("looking up lat/long for " + city);
+    localStorage.setItem("city", city);
     var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=9ca09e5fa97a5b754fdff55f82acba19";
     fetch(geoURL)
     .then(function(response) {
@@ -21,18 +22,65 @@ function getLatLong(city) {
 
 function getWeather(lat, lon) {
     console.log("looking up weather for " + lat + ", " + lon);
-    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=9ca09e5fa97a5b754fdff55f82acba19";
+
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=9ca09e5fa97a5b754fdff55f82acba19&units=metric";
     fetch(weatherURL)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
-        var currentTemp = data.main.temp;
-        var currentHumidity = data.main.humidity;
-        var currentWindSpeed = data.wind.speed;
-        
+        if (data.length === 0) {
+            console.log("no results");
+            alert("No results found. Please try again.");
+        } else {
+            loadCurrentWeather(data);
+        }
     })
+}
+
+function loadCurrentWeather(data) {
+
+    console.log("loading current weather for " + data.name);
+
+    var cityName = data.name;
+    var currentDate = dayjs().format("MM/DD/YYYY");
+    var currentIcon = data.weather[0].icon;
+    var currentTemp = data.main.temp;
+    var currentHumidity = data.main.humidity;
+    var currentWindSpeed = data.wind.speed;
+
+    var currentWeather = document.getElementById("Current Weather");
+    var cityNameEl = document.createElement("h1");
+    var currentIconEl = document.createElement("img");
+    var currentTempEl = document.createElement("p");
+    var currentHumidityEl = document.createElement("p");
+    var currentWindSpeedEl = document.createElement("p");
+    
+    currentWeather.appendChild(cityNameEl);
+    cityNameEl.textContent = cityName + " (" + currentDate + ")";
+    cityNameEl.appendChild(currentIconEl);
+    currentIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + currentIcon + ".png");
+    currentWeather.appendChild(currentTempEl);
+    currentTempEl.textContent = "Temperature: " + currentTemp + "°C";
+    currentWeather.appendChild(currentHumidityEl);
+    currentHumidityEl.textContent = "Humidity: " + currentHumidity + "%";
+    currentWeather.appendChild(currentWindSpeedEl);
+    currentWindSpeedEl.textContent = "Wind Speed: " + currentWindSpeed + " KPH";
+
+    var day1 = document.getElementById("Day 1");
+    day1.setAttribute("style", "display: block");
+    
+    var day2 = document.getElementById("Day 2");
+    day2.setAttribute("style", "display: block");
+    var day3 = document.getElementById("Day 3");
+    day3.setAttribute("style", "display: block");
+    var day4 = document.getElementById("Day 4");
+    day4.setAttribute("style", "display: block");
+    var day5 = document.getElementById("Day 5");
+    day5.setAttribute("style", "display: block");
+
+
 }
 
 
@@ -40,6 +88,7 @@ function getWeather(lat, lon) {
 searchButton.addEventListener("click", function() {
     event.preventDefault();
     var city = searchInput.value;
+
     if (city==="") {
         alert("Please enter a city");
         return;}
